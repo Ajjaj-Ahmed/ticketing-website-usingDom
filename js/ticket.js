e140d4eccb1e6dcd6c12fb037e11d4dbc190e354
Ajjaj-Ahmed <ajjajahmedasif@gmail.com>
2024-02-20 13:14:57 +0600

    adding seat

--- js/ticket.js
+++ js/ticket.js
@@ -1,3 +1,21 @@
-function getSeatNumberElementById(){
-  
+
+function addChildToSeatTable(elementId){
+  let newTr = document.createElement('tr');
+        
+  //create cell
+  let seatNumber = document.createElement('td'); // have work with this row
+  seatNumber.textContent = elementId;
+  let seatClass = document.createElement('td');
+  seatClass.textContent = 'Economy';
+  let seatFare = document.createElement('td');
+  seatFare.textContent = 550
+
+  // Append cells to the new row
+  newTr.appendChild(seatNumber);
+  newTr.appendChild(seatClass);
+  newTr.appendChild(seatFare);
+
+  let table = document.getElementById('myTable');
+  table.appendChild(newTr);
+
 }
--- js/utility.js
+++ js/utility.js
@@ -1,26 +1,84 @@
 
-const seats = document.getElementsByClassName('seats')
-for(const seat of seats){
-  seat.addEventListener('click',function handleSelect(event){
-    const eventList= event.target;
-    // console.log(eventList);
-    const eventText = eventList.innerText;
-    console.log(eventText)
-    addElementToSeat(eventText);
-    eventList.style.backgroundColor="green";
-    
-  })
+const seats = document.getElementsByClassName('seatBtn')
+
+let evenTextArray = [];
+
+//step:one
+
+  for(const seat of seats){
+    seat.addEventListener('click',function handleSelect(event){
+      const userId =event.target.id;
+      const eventList= event.target;
+      const eventText = eventList.innerText;
+
+      if(evenTextArray.includes(eventText) === true){
+        // increase the available seat number
+        removeBackgroundColor(userId);
+        increaseTheDecreseValue();
+        decreaseTheIncreaseValue();
+      } 
+      else{
+        setBackgroundColor(userId);
+
+        addChildToSeatTable(eventText);
+
+        evenTextArray.push(eventText);
+      }
+
+  
+      //increase and decrease score
+      increaseScore('seatNumber');
+      decreaseScore('availableSeat'); 
+  
+    })
+  }
+
+
+
+
+
+//step: 3 increase seat number
+function increaseScore(elementId){
+  let elementSeat = document.getElementById(elementId);
+  const elementText=elementSeat.innerText;
+  const element = parseInt(elementText);
+  const elementNumber = element+1;
+  elementSeat.innerText = elementNumber;
+
+} 
+
+
+// step : 4 decrease seat number
+ function decreaseScore(elementId){
+  let elementSeat = document.getElementById(elementId);
+  const elementText=elementSeat.innerText;
+  const element = parseInt(elementText);
+  const elementNumber = element-1;
+  elementSeat.innerText = elementNumber;
+} 
+
+function increaseTheDecreseValue(){
+  const elementText = document.getElementById('availableSeat').innerText;
+  const element = parseInt(elementText);
+  const number = element+2;
+  document.getElementById('availableSeat').innerText = number;
 }
 
-function addElementToSeat(eventText){
-  const mainContainer = document.getElementById('seatContainer');
-  mainContainer.innerHTML=`${eventText} <p>Economy</p> <p>550</p>`;
-  handleSelect(event);
-  /* const para = document.createElement('h2');
-  para.innerText = eventText;
-  console.log(para)
-  mainContainer.appendChild(para); */
+function decreaseTheIncreaseValue(){
+  const elementText = document.getElementById('seatNumber').innerText;
+  const element = parseInt(elementText);
+  const number = element-2;
+  document.getElementById('seatNumber').innerText = number;
 }
-// next problem is append chail in left side
+ 
 
-
+//background color
+function setBackgroundColor(elementId){
+  const selectElement = document.getElementById(elementId);
+  selectElement.classList.add('bg-orange-400');
+  }
+  function removeBackgroundColor(elementId){
+  const selectElement = document.getElementById(elementId);
+  selectElement.classList.remove('bg-orange-400');
+  }
+  
